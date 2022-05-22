4ce5e94dc31630ec09a82f0591b66ad16b731791
Mattox Beckman <mattox@illinois.edu>
2022-05-22 17:06:54 -0500

    Release MP 2

---
+++ mp2-interpreter/src/Lib.hs
@@ -0,0 +1,160 @@
+module Lib where
+import Data.HashMap.Strict as H (HashMap, empty, fromList, insert, lookup, union)
+
+
+--- Data Types
+--- ----------
+
+--- ### Environments and Results
+
+type Env  = H.HashMap String Val
+type PEnv = H.HashMap String Stmt
+
+type Result = (String, PEnv, Env)
+
+--- ### Values
+
+data Val = IntVal Int
+         | BoolVal Bool
+         | CloVal [String] Exp Env
+         | ExnVal String
+    deriving (Eq)
+
+instance Show Val where
+    show (IntVal i) = show i
+    show (BoolVal i) = show i
+    show (CloVal xs body env) = "<" ++ show xs   ++ ", "
+                                    ++ show body ++ ", "
+                                    ++ show env  ++ ">"
+    show (ExnVal s) = "exn: " ++ s
+
+--- ### Expressions
+
+data Exp = IntExp Int
+         | BoolExp Bool
+         | FunExp [String] Exp
+         | LetExp [(String,Exp)] Exp
+         | AppExp Exp [Exp]
+         | IfExp Exp Exp Exp
+         | IntOpExp String Exp Exp
+         | BoolOpExp String Exp Exp
+         | CompOpExp String Exp Exp
+         | VarExp String
+    deriving (Show, Eq)
+
+--- ### Statements
+
+data Stmt = SetStmt String Exp
+          | PrintStmt Exp
+          | QuitStmt
+          | IfStmt Exp Stmt Stmt
+          | ProcedureStmt String [String] Stmt
+          | CallStmt String [Exp]
+          | SeqStmt [Stmt]
+    deriving (Show, Eq)
+
+--- Primitive Functions
+--- -------------------
+
+intOps :: H.HashMap String (Int -> Int -> Int)
+intOps = H.fromList [ ("+", (+))
+                    , ("-", (-))
+                    , ("*", (*))
+                    , ("/", (div))
+                    ]
+
+boolOps :: H.HashMap String (Bool -> Bool -> Bool)
+boolOps = H.fromList [ ("and", (&&))
+                     , ("or", (||))
+                     ]
+
+compOps :: H.HashMap String (Int -> Int -> Bool)
+compOps = H.fromList [ ("<", (<))
+                     , (">", (>))
+                     , ("<=", (<=))
+                     , (">=", (>=))
+                     , ("/=", (/=))
+                     , ("==", (==))
+                     ]
+
+--- Problems
+--- ========
+
+--- Lifting Functions
+--- -----------------
+
+liftIntOp :: (Int -> Int -> Int) -> Val -> Val -> Val
+liftIntOp op (IntVal x) (IntVal y) = IntVal $ op x y
+liftIntOp _ _ _ = ExnVal "Cannot lift"
+
+liftBoolOp :: (Bool -> Bool -> Bool) -> Val -> Val -> Val
+liftBoolOp = undefined
+
+liftCompOp :: (Int -> Int -> Bool) -> Val -> Val -> Val
+liftCompOp = undefined
+
+--- Eval
+--- ----
+
+eval :: Exp -> Env -> Val
+
+--- ### Constants
+
+eval (IntExp i)  _ = undefined
+eval (BoolExp i) _ = undefined
+
+--- ### Variables
+
+eval (VarExp s) env = undefined
+
+--- ### Arithmetic
+
+eval (IntOpExp op e1 e2) env = undefined
+
+--- ### Boolean and Comparison Operators
+
+eval (BoolOpExp op e1 e2) env = undefined
+
+eval (CompOpExp op e1 e2) env = undefined
+
+--- ### If Expressions
+
+eval (IfExp e1 e2 e3) env = undefined
+
+--- ### Functions and Function Application
+
+eval (FunExp params body) env = undefined
+
+eval (AppExp e1 args) env = undefined
+
+--- ### Let Expressions
+
+eval (LetExp pairs body) env = undefined
+
+--- Statements
+--- ----------
+
+-- Statement Execution
+-- -------------------
+
+exec :: Stmt -> PEnv -> Env -> Result
+exec (PrintStmt e) penv env = (val, penv, env)
+    where val = show $ eval e env
+
+--- ### Set Statements
+
+exec (SetStmt var e) penv env = undefined
+
+--- ### Sequencing
+
+exec (SeqStmt []) penv env = undefined
+
+--- ### If Statements
+
+exec (IfStmt e1 s1 s2) penv env = undefined
+
+--- ### Procedure and Call Statements
+
+exec p@(ProcedureStmt name args body) penv env = undefined
+
+exec (CallStmt name args) penv env = undefined
